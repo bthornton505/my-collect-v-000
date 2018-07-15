39884cdd5545ec09b8a57ef6e2d9614887e254fb
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-15 23:25:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+    i = 0 
+    collections = []
+    while i < collection.length 
+      collections << yield(collection[i])
+      i += 1 
+    end 
+    collection
+end 
 
